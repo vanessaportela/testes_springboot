0cc3f880c2a64dcf2e05eb4019853a441be4468d
vanessaportela <vgportela@outlook.com>
2023-02-22 14:55:43 -0300

    tratamento de exceção para objeto não encontrado

---
+++ api/src/main/java/br/com/vanessa/api/resources/exceptions/StandardError.java
@@ -0,0 +1,17 @@
+package br.com.vanessa.api.resources.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class StandardError {
+    private LocalDateTime timestamp;
+    private Integer status;
+    private String Error;
+    private String path;
+}
--- api/src/main/java/br/com/vanessa/api/service/impl/UserServiceImpl.java
+++ api/src/main/java/br/com/vanessa/api/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.vanessa.api.service.impl;
 import br.com.vanessa.api.domain.User;
 import br.com.vanessa.api.repositories.UserRepository;
 import br.com.vanessa.api.service.UserService;
+import br.com.vanessa.api.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
