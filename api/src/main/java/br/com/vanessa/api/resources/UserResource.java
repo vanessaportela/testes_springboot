670027a74621264267316a701a29e785b28a3ae1
vanessaportela <vgportela@outlook.com>
2023-02-22 11:43:01 -0300

    classe user resource findById

--- api/src/main/java/br/com/vanessa/api/resources/UserResource.java
+++ api/src/main/java/br/com/vanessa/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.vanessa.api.resources;
 
 import br.com.vanessa.api.domain.User;
+import br.com.vanessa.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,11 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
 
-        return ResponseEntity.ok().body(new User(1, "Valdir", "val@mail.com", "123"));
-
+        return ResponseEntity.ok().body(service.findById(id));
     }
-
 }
