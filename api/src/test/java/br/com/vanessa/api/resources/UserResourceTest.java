d4d8b683c157b2b33773dda64e0325ddb614f8f3
vanessaportela <vgportela@outlook.com>
2023-02-23 13:57:11 -0300

    teste para endpoint findById da classe user resource implementado

--- api/src/test/java/br/com/vanessa/api/resources/UserResourceTest.java
+++ api/src/test/java/br/com/vanessa/api/resources/UserResourceTest.java
@@ -11,13 +11,21 @@ import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
 @SpringBootTest
 class UserResourceTest {
 
-    private static final Integer ID      = 1;
-    private static final Integer INDEX   = 0;
-    private static final String NAME     = "Valdir";
-    private static final String EMAIL    = "valdir@mail.com";
+    private static final Integer ID = 1;
+    private static final Integer INDEX = 0;
+    private static final String NAME = "Valdir";
+    private static final String EMAIL = "valdir@mail.com";
     private static final String PASSWORD = "123";
 
     private User user;
@@ -39,7 +47,21 @@ class UserResourceTest {
     }
 
     @Test
-    void findById() {
+    void whenFindByIdThenReturnSuccess() {
+        when(service.findById(anyInt())).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resource.findById(ID);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME, response.getBody().getName());
+        assertEquals(EMAIL, response.getBody().getEmail());
+        assertEquals(PASSWORD, response.getBody().getPassword());
     }
 
     @Test
@@ -59,7 +81,7 @@ class UserResourceTest {
     }
 
     private void startUser() {
-        user = new User(ID, NAME, EMAIL,  PASSWORD);
+        user = new User(ID, NAME, EMAIL, PASSWORD);
         userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
     }
 }
